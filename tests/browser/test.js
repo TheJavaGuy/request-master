2bad361a2f0fb574f46194d01cb2c52f46f9fe34
Ivan Milosavljevic (TheJavaGuy) <TheJavaGuy@yandex.com>
2020-10-27 16:31:24 +0100

    replace PhantomJS with headless Chrome
    PhantomJS is dead for a long time. This commit replaces it with headless Chrome over puppeteer. Also, it updates all testing related dependencies.

--- tests/browser/test.js
+++ tests/browser/test.js
@@ -1,13 +1,5 @@
 'use strict';
 
-if (!Function.prototype.bind) {
-  // This is because of the fact that phantom.js does not have Function.bind.
-  // This is a bug in phantom.js.
-  // More info: https://github.com/ariya/phantomjs/issues/10522
-  /* eslint no-extend-native:0 */
-  Function.prototype.bind = require('function-bind');
-}
-
 var tape = require('tape');
 var request = require('../../index');
 
